221b674cf7a5311c1d0de2e4ca07daa9b7ab3e20
Ollie Farley <o.j.d.farley@durham.ac.uk>
2022-04-26 15:25:21 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+with open("README.rst", "r") as readme:
+    long_description= readme.read()
+
+setup(
+    name='fast',
+    author_email='o.j.d.farley@durham.ac.uk',
+    url='https://github.com/ojdf/fast',
+    packages=['fast'],
+
+    description='Fourier domain Adaptive Optics Simulation Tool',
+    long_description=long_description,
+    version='0.1'
+)
