c452eeebf3b5c7eef8a53242664a0faa4eb56781
Ollie Farley <o.j.d.farley@durham.ac.uk>
2024-06-20 15:25:34 +0100

    completely refactored config

--- fast/conf.py
+++ fast/conf.py
@@ -5,28 +5,35 @@ default values for certain parameters.
 import importlib.util
 import numpy
 import logging
+from collections import UserDict
+import copy
 
 logger = logging.getLogger(__name__)
 
 class ConfigParser():
+    """
+    Config file parser for FAST. 
 
-    def __init__(self, fname_or_dict):
+    Parameters:
+        arg (str or FastConfig): Either a file name of the config file, or a previously 
+            created FastConfig object 
+    """
+    def __init__(self, arg):
         
-        if type(fname_or_dict) == dict:
-            self.config = fname_or_dict
+        self.defaults = []
+        self.config = FastConfig()
+        self.init_params()
+
+        if type(arg) == FastConfig:
+            for key in arg:
+                self.config[key] = arg[key]
             self.fname = None
-        elif type(fname_or_dict) == str:
-            self.fname = fname_or_dict
-            self.config = {}
-            self.load(fname_or_dict)
+        elif type(arg) == str:
+            self.fname = arg
+            self.load(arg)
         else:
             raise Exception("Either config file name or params dict required")
-
-        self.defaults = {}
-        self.set_defaults()
-
-        self.check()
-        
+                
     def load(self, fname):
         '''
         Load config file into dictionary
@@ -40,77 +47,211 @@ class ConfigParser():
             spec = importlib.util.spec_from_file_location("", fname)
             conf_module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(conf_module)
-            self.config = conf_module.p
+            
+            for key in conf_module.p:
+                self.config[key] = conf_module.p[key]
+
             return
 
         raise Exception("Require .py config file")
 
-    def check(self):
+    def init_params(self):
         '''
-        Check loaded config dictionary for missing values. If they exist, replace 
-        with defualt values
+        Set defualt values for all config parameters from global PARAMS list
         '''
-        for key in self.defaults.keys():
-            try:
-                self.config[key]
-            except KeyError:
-                logger.warning(f"Config parameter {key} not defined in {self.fname}, setting default value of {self.defaults[key]}")
-                self.config[key] = self.defaults[key]
-
-    def set_defaults(self):
-        '''
-        Set defualt values for all config parameters from global DEFAULTS dictionary
-        '''
-        self.defaults = DEFAULTS
+        self.defaults = PARAMS
+        for d in self.defaults:
+            self.config[d.name] = d
 
 
-DEFAULTS = {'NPXLS': 'auto',
-            'DX': 'auto',
-            'NITER': 1000,
-            'SUBHARM': False,
-            'FFTW': False,
-            'FFTW_THREADS': 1,
-            'NCHUNKS': 10,
-            'TEMPORAL': False,
-            'DT': 0.001,
-            'LOGFILE': None,
-            'LOGLEVEL': "INFO",
-            'SEED': None,   
+class FastConfig(UserDict):
+    '''
+    Wrapper around a dict of FastParam parameter values. 
+    '''
+    def __getitem__(self, key):
+        return self.data[key].value
 
-            'W0': "opt",               
-            'D_GROUND': 1.0,   
-            'OBSC_GROUND': 0,   
-            'D_SAT': 0.1,                           
-            'OBSC_SAT': 0,                     
-            'WVL': 1550e-9,        
-            'AXICON': False,                           
-            'POWER': 1,                                 
-            'SMF': True,                                
+    def __setitem__(self, key, value):
+        try:
+            self.data[key].value = value
+        except KeyError:
+            self.data.__setitem__(key, value)
 
-            'H_SAT': 36e6,
-            'L_SAT': None,
-            'H_TURB': numpy.array([ 0, 10e3]),
-            'CN2_TURB': numpy.array([100e-15, 100e-15]),
-            'WIND_SPD': numpy.array([ 10, 10]),
-            'WIND_DIR': numpy.array([90., 0.]),
-            'L0': numpy.inf,
-            'l0': 1e-06,
-            'ZENITH_ANGLE': 0,
-            'PROP_DIR': 'up',
-            'DTHETA': [4,0],    
-            'TRANSMISSION': 1,
+    def copy(self):
+        return copy.deepcopy(self)
+    
+    def __repr__(self):
+        return self.data.__repr__()
 
-            'AO_MODE': 'AO',
-            'DSUBAP': 0.02,
-            'TLOOP': 0.001,
-            'TEXP': 0.001,
-            'ALIAS': True,
-            'NOISE': 0.0,
-            'MODAL': False,
-            'MODAL_MULT': 1,
-            'ZMAX': None,
+class FastParam():
+    """
+    FAST config parameter class. 
 
-            'COHERENT': False,
-            'MODULATION': None, 
-            'EsN0': None}
+    Parameters:
+        name (str): name of the parameter
+        value: (initial) value for the parameter
+        category (str): category of the parameter. For compatibility with future 
+            versions where I organise the config files a bit better.
+        bounds (tuple): (min, max) allowed values for the parameter. None indicated 
+            no bound 
+        allowed_values (list): allowed non-numeric values (e.g. ["opt", None])
+        required_len (int or None): required length if an array parameter.
+
+    """
+    def __init__(self, name, value, category, bounds=[None, None], 
+                 allowed_values=[], required_len=None, unit=None, info=""):
+
+        self._bounds = bounds 
+        if self._bounds[0] == None: self._bounds[0] = -numpy.inf 
+        if self._bounds[1] == None: self._bounds[1] = +numpy.inf
+
+        self._allowed_values = allowed_values
+        self._category = category
+        self._required_len = required_len
+
+        self.name = name
+        self.value = value
+        self.unit = unit 
+        self.info = info
+
+    @property
+    def value(self):
+        return self._value 
+
+    @value.setter
+    def value(self, v):
+        self.check(v)
+        self._value = v
+
+    def check(self, v):
+        
+        if isinstance(v, (numpy.ndarray, list, tuple)):
+            v = numpy.array(v)
+            if not ((v >= self._bounds[0]).all() and (v <= self._bounds[1]).all()):
+                raise ValueError(f"One or more values in parameter {self.name} out of bounds {self._bounds}")
+            
+            if self._required_len != None: 
+                if len(v) != self._required_len:
+                    raise ValueError(f"Parameter {self.name} cannot have length {len(v)}, must be {self._required_len}")
+
+        elif isinstance(v, (int, float)):
+            if not (v >= self._bounds[0] and v <= self._bounds[1]):
+                raise ValueError(f"Parameter {self.name} out of bounds {self._bounds}")
+            
+        elif isinstance(v, str) or (v == None):
+            if not v in self._allowed_values and "*" not in self._allowed_values:
+                raise ValueError(f"Parameter {self.name} can only have values {self._allowed_values}, not {v}")
+            
+    def __repr__(self):
+        if self.unit is None or self.value is None:
+            u = ""
+        else:
+            u = self.unit
+        out = f"<FAST Parameter {self.name}={self.value} {u}>"
+        return out
+
+# Master list of all FAST params, including default values, bounds, allowed values, etc.
+PARAMS = [
+    FastParam("NPXLS", "auto", "sim", bounds=[0,None], allowed_values=["auto"]),
+    FastParam("DX", "auto", "sim", bounds=[0,None], allowed_values=["auto"], unit="m/pixel"),
+    FastParam("NITER", 1000, "sim", bounds=[0,None]), 
+    FastParam("SUBHARM", False, "sim"),
+    FastParam("FFTW", False, "sim"),
+    FastParam("FFTW_THREADS", 1, "sim", bounds=[0,None]), 
+    FastParam("NCHUNKS", 10, "sim", bounds=[0,None]),
+    FastParam("TEMPORAL", False, "sim"), 
+    FastParam("DT", 0.001, "sim", bounds=[0,None], unit="s"), 
+    FastParam("LOGFILE", None, "sim", allowed_values=[None, "*"]),
+    FastParam("LOGLEVEL", "INFO", "sim", allowed_values=["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+    FastParam("SEED", None, "sim", allowed_values=[None]),
+    
+    FastParam("W0", "opt", "ground", bounds=[0,None], allowed_values=["opt"], unit="m"),
+    FastParam("D_GROUND", 1.0, "ground", bounds=[0,None], unit="m"),
+    FastParam("OBSC_GROUND", 0.0, "ground", bounds=[0,None], unit="m"),
+    FastParam("D_SAT", 0.1, "sat", bounds=[0,None], unit="m"),
+    FastParam("OBSC_SAT", 0, "sat", bounds=[0,None], unit="m"),
+    FastParam("WVL", 1550e-9, "rx/tx", bounds=[0,None], unit="m"),
+    FastParam("AXICON", False, "ground"),
+    FastParam("POWER", 1, "tx", bounds=[0,None], unit="Watts"),
+    FastParam("SMF", True, "rx"),
+
+    FastParam("H_SAT", 36e6, "sat", bounds=[0,None], allowed_values=[None], unit="m"),
+    FastParam("L_SAT", None, "sat", bounds=[0,None], allowed_values=[None], unit="m"), 
+    FastParam("H_TURB", numpy.array([0,10e3]), "turb", bounds=[0,None], unit="m"),
+    FastParam("CN2_TURB", numpy.array([100e-15, 100e-15]), "turb", bounds=[0,None], unit="m^1/3"),
+    FastParam("WIND_SPD", numpy.array([10,10]), "turb", bounds=[0,None], unit="m/s"),
+    FastParam("WIND_DIR", numpy.array([90,0]), "turb", unit="degrees"),
+    FastParam("L0", numpy.inf, "turb", bounds=[0,None], unit="m"),
+    FastParam("l0", 1e-6, "turb", bounds=[0,None], unit="m"),
+    FastParam("ZENITH_ANGLE", 0, "turb", bounds=[0,90], unit="degrees"),
+    FastParam("PROP_DIR", "up", "link", allowed_values=["up", "down"]),
+    FastParam("DTHETA", [4,0], "linK", required_len=2, unit="arcseconds"),
+    FastParam("TRANSMISSION", 1, "link", bounds=[0,1]),
+
+    FastParam("AO_MODE", "AO", "ao", allowed_values=["NOAO", "AO", "TT", "LGSAO"]),
+    FastParam("DSUBAP", 0.1, "ao", bounds=[0,None], unit="m"),
+    FastParam("TLOOP", 0.001, "ao", bounds=[0,None], unit="s"), 
+    FastParam("TEXP", 0.001, "ao", bounds=[0,None], unit="s"),
+    FastParam("ALIAS", True, "ao"),
+    FastParam("NOISE", 0.0, "ao", bounds=[0,None], unit="?"), 
+    FastParam("MODAL", False, "ao"),
+    FastParam("MODAL_MULT", 1, "ao", bounds=[0,None]),
+    FastParam("ZMAX", None, "ao", bounds=[1,None], allowed_values=[None]),
+
+    FastParam("COHERENT", False, "comms"),
+    FastParam("MODULATION", None, "comms", allowed_values=[None, "OOK", "BPSK", "QAM", "QPSK", "*"]),
+    FastParam("EsN0", None, "comms", allowed_values=[None], unit="dB")
+]
+
+# Default values for all parameters. Each entry contains 
+# "PARAMETER": [<default value>, <(allowed types)>, <category>]
+# DEFAULTS = {'NPXLS': ['auto', (str, int), "sim"],
+#             'DX': ['auto', (str, float), "sim"],
+#             'NITER': [1000, (int), "sim"],
+#             'SUBHARM': [False, (bool), "sim"],
+#             'FFTW': [False, (bool), "sim"],
+#             'FFTW_THREADS': [1, (int), "sim"],
+#             'NCHUNKS': [10, (int), "sim"],
+#             'TEMPORAL': [False, (bool), "sim"],
+#             'DT': [0.001, (float), "sim"],
+#             'LOGFILE': [None, (None, str), "sim"],
+#             'LOGLEVEL': ["INFO", (str), "sim"],
+#             'SEED': [None, (None, int), "sim"],   
+
+#             'W0': ["opt", (str, float, int), "rx/tx"],               
+#             'D_GROUND': [1.0, (float, int), "rx/tx"],   
+#             'OBSC_GROUND': [0, (float, int), "rx/tx"],   
+#             'D_SAT': [0.1, (float, int), "rx/tx"],                           
+#             'OBSC_SAT': [0, (float, int), "rx/tx"],                     
+#             'WVL': [1550e-9, (float), "rx/tx"],        
+#             'AXICON': [False, (bool), "rx/tx"],                           
+#             'POWER': [1, (float, int), "rx/tx"],                                 
+#             'SMF': [True, (bool), "rx/tx"],                                
+
+#             'H_SAT': [36e6, (None, float, int), "turb/link"],
+#             'L_SAT': [None, (None, float, int), "turb/link"],
+#             'H_TURB': [numpy.array([ 0, 10e3]), (list, numpy.ndarray), "turb/link"],
+#             'CN2_TURB': [numpy.array([100e-15, 100e-15]), (list, numpy.ndarray), "turb/link"],
+#             'WIND_SPD': [numpy.array([ 10, 10]), (list, numpy.ndarray), "turb/link"],
+#             'WIND_DIR': [numpy.array([90., 0.]), (list, numpy.ndarray), "turb/link"],
+#             'L0': [numpy.inf, (float, int), "turb/link"],
+#             'l0': [1e-06, (float, int), "turb/link"],
+#             'ZENITH_ANGLE': [0, (float, int), "turb/link"],
+#             'PROP_DIR': ['up', (str), "turb/link"],
+#             'DTHETA': [[4,0], (list, numpy.ndarray), "turb/link"],    
+#             'TRANSMISSION': [1, (float, int), "turb/link"],
+
+#             'AO_MODE': 'AO',
+#             'DSUBAP': 0.02,
+#             'TLOOP': 0.001,
+#             'TEXP': 0.001,
+#             'ALIAS': True,
+#             'NOISE': 0.0,
+#             'MODAL': False,
+#             'MODAL_MULT': 1,
+#             'ZMAX': None,
+
+#             'COHERENT': False,
+#             'MODULATION': None, 
+#             'EsN0': None}
 
--- test/tests_pytest.py
+++ test/tests_pytest.py
@@ -2,7 +2,7 @@ import pytest
 import fast 
 import numpy
 
-p = None
+P = None
 def run_sim(p):
     sim = fast.Fast(p)
     sim.run()
@@ -27,17 +27,31 @@ def test_HV57_Bufton():
     assert len(h) == len(cn2) == len(w)
 
 
-# Test config parsing 
+# Test config parsing and data checking 
 def test_config_default():
     c = fast.conf.ConfigParser("test/test_params.py")
 
+def test_config_bounds():
+    c = fast.conf.ConfigParser("test/test_params.py")
+    with pytest.raises(ValueError):
+        c.config['W0'] = -1
+    
+def test_config_allowed():
+    c = fast.conf.ConfigParser("test/test_params.py")
+    with pytest.raises(ValueError):
+        c.config['NPXLS'] = "hello"
+    
+def test_config_max_len():
+    c = fast.conf.ConfigParser("test/test_params.py")
+    with pytest.raises(ValueError):
+        c.config['DTHETA'] = [1,2,3]
 
 # Test complete simulation
 def test_sim_default():
     sim = fast.Fast("test/test_params.py")
     sim.run()
-    global p
-    p = sim.params.copy()
+    global P
+    P = sim.params.copy()
     assert numpy.isfinite(sim.result.power).all()
     assert numpy.isfinite(sim.result.dB_rel).all()
     assert numpy.isfinite(sim.result.dB_abs).all()
@@ -49,87 +63,87 @@ def test_sim_mean_irradiance():
 
 @pytest.mark.skipif(not fast.fast._pyfftw, reason="No pyfftw installed")
 def test_sim_fftw():
-    ptmp = p.copy()
-    ptmp['FFTW'] = True
-    run_sim(ptmp)
-
-def test_sim_randomScrns():
-    ptmp = p.copy()
-    ptmp['TEMPORAL'] = False
-    run_sim(ptmp)
-
-def test_sim_subharm():
-    ptmp = p.copy()
-    ptmp['SUBHARM'] = True
-    ptmp['TEMPORAL'] = False
-    run_sim(ptmp)
-
-def test_sim_obsc():
-    ptmp = p.copy()
-    ptmp['OBSC_GROUND'] = 0.1
-    run_sim(ptmp)
-
-def test_sim_obsc_sat():
-    ptmp = p.copy()
-    ptmp['OBSC_SAT'] = 0.05
-    run_sim(ptmp)
-
-def test_sim_axicon():
-    ptmp = p.copy()
-    ptmp['W0'] = 0.1
-    ptmp['AXICON'] = True
-    ptmp['OBSC_GROUND'] = 0.1
-    run_sim(ptmp)
-
-def test_sim_L_SAT():
-    ptmp = p.copy()
-    ptmp['L_SAT'] = 500e3
-    sim = fast.Fast(ptmp)
-    assert sim.L == ptmp['L_SAT']
-
-def test_sim_L0():
-    ptmp = p.copy()
-    ptmp['L0'] = 25
-    run_sim(ptmp)
-
-def test_sim_down():
-    ptmp = p.copy()
-    p['PROP_DIR'] = 'down'
+    p = P.copy()
+    p['FFTW'] = True
     run_sim(p)
 
+def test_sim_randomScrns():
+    p = P.copy()
+    p['TEMPORAL'] = False
+    run_sim(p)
+
+def test_sim_subharm():
+    p = P.copy()
+    p['SUBHARM'] = True
+    p['TEMPORAL'] = False
+    run_sim(p)
+
+def test_sim_obsc():
+    p = P.copy()
+    p['OBSC_GROUND'] = 0.1
+    run_sim(p)
+
+def test_sim_obsc_sat():
+    p = P.copy()
+    p['OBSC_SAT'] = 0.05
+    run_sim(p)
+
+def test_sim_axicon():
+    p = P.copy()
+    p['W0'] = 0.1
+    p['AXICON'] = True
+    p['OBSC_GROUND'] = 0.1
+    run_sim(p)
+
+def test_sim_L_SAT():
+    p = P.copy()
+    p['L_SAT'] = 500e3
+    sim = fast.Fast(p)
+    assert sim.L == p['L_SAT']
+
+def test_sim_L0():
+    p = P.copy()
+    p['L0'] = 25
+    run_sim(p)
+
+def test_sim_down():
+    p = P.copy()
+    p['PROP_DIR'] = 'down'
+    run_sim(P)
+
 def test_sim_NOAO():
-    ptmp = p.copy()
-    ptmp['AO_MODE'] = 'NOAO'
-    run_sim(ptmp)
+    p = P.copy()
+    p['AO_MODE'] = 'NOAO'
+    run_sim(p)
 
 def test_sim_TT():
-    ptmp = p.copy()
-    ptmp['AO_MODE'] = 'TT'
-    run_sim(ptmp)
+    p = P.copy()
+    p['AO_MODE'] = 'TT'
+    run_sim(p)
 
 def test_sim_noise():
-    ptmp = p.copy()
+    p = P.copy()
     p['NOISE'] = 1
-    run_sim(ptmp)
+    run_sim(p)
 
 def test_sim_modal():
-    ptmp = p.copy()
-    ptmp['MODAL'] = True
-    run_sim(ptmp)
+    p = P.copy()
+    p['MODAL'] = True
+    run_sim(p)
 
 
 # Test comms 
 def test_sim_coherent():
-    ptmp = p.copy()
-    ptmp['COHERENT'] = True
-    sim = fast.Fast(ptmp)
+    p = P.copy()
+    p['COHERENT'] = True
+    sim = fast.Fast(p)
     sim.run()
     assert sim.I.dtype == complex
 
 def test_fast_fsoc_ook():
-    ptmp = p.copy()
-    ptmp['MODULATION'] = "OOK"
-    sim = fast.comms.FastFSOC(ptmp)
+    p = P.copy()
+    p['MODULATION'] = "OOK"
+    sim = fast.comms.FastFSOC(p)
     sim.run()
     assert numpy.isfinite(sim.I).all()
     assert hasattr(sim.modulator, "sep")
@@ -137,9 +151,9 @@ def test_fast_fsoc_ook():
     assert numpy.isfinite(sim.modulator.evm)
 
 def test_fast_fsoc_BPSK():
-    ptmp = p.copy()
-    ptmp['MODULATION'] = "BPSK"
-    sim = fast.comms.FastFSOC(ptmp)
+    p = P.copy()
+    p['MODULATION'] = "BPSK"
+    sim = fast.comms.FastFSOC(p)
     sim.run()
     assert numpy.isfinite(sim.I).all()
     assert hasattr(sim.modulator, "sep")
@@ -147,9 +161,9 @@ def test_fast_fsoc_BPSK():
     assert numpy.isfinite(sim.modulator.evm)
 
 def test_fast_fsoc_qam():
-    ptmp = p.copy()
-    ptmp['MODULATION'] = "QAM"
-    sim = fast.comms.FastFSOC(ptmp)
+    p = P.copy()
+    p['MODULATION'] = "QAM"
+    sim = fast.comms.FastFSOC(p)
     sim.run()
     assert numpy.isfinite(sim.I).all()
     assert hasattr(sim.modulator, "sep")
@@ -158,16 +172,16 @@ def test_fast_fsoc_qam():
 
 # def test_fast_fsoc_ook_withdata():
 #     data = numpy.random.normal(size=100).tobytes()
-#     ptmp = p.copy()
-#     sim = fast.Fast(ptmp)
+#     p = p.copy()
+#     sim = fast.Fast(p)
 #     sim.run()
 #     mod = fast.comms.Modulator(sim.result.power, 'OOK', 10, data=data, symbols_per_iter=len(data))
 #     mod.run()
 #     assert numpy.isfinite(sim.recv_symbols)
 
 def test_ber_ook():
-    ptmp = p.copy()
-    sim = fast.Fast(ptmp)
+    p = P.copy()
+    sim = fast.Fast(p)
     sim.run()
     ber = fast.comms.ber_ook(10, sim.result.power)
 
@@ -176,8 +190,8 @@ def test_ber_ook_nosamples():
     assert numpy.isfinite(ber)
 
 def test_sep_qam():
-    ptmp = p.copy()
-    sim = fast.Fast(ptmp)
+    p = P.copy()
+    sim = fast.Fast(p)
     sim.run()
     ber = fast.comms.ber_qam(4, 10, samples=sim.result.power)
     assert numpy.isfinite(ber)
@@ -190,10 +204,10 @@ def test_ber_qam_nosamples():
 def test_complete_orbit_simulation():
     import test_satellite_param
 
-    ptmp = p.copy()
+    p = P.copy()
     p_orbit = test_satellite_param.p_simu.copy()
     tle_path = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle'
 
     simulation_test = None
-    simulation_test = fast.complete_orbit_simulation.FAST_sat_orbit(ptmp, p_orbit, tle_path)
+    simulation_test = fast.complete_orbit_simulation.FAST_sat_orbit(p, p_orbit, tle_path)
     assert simulation_test[list(simulation_test.keys())[0]]
