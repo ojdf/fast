41c2e5b556fa313167cca1fe625825457442045e
Ollie Farley <o.j.d.farley@durham.ac.uk>
2024-07-03 17:28:51 +0100

    testing gui

---
+++ gui/gui.py
@@ -0,0 +1,150 @@
+from nicegui import events, ui 
+import fast
+import numpy
+
+CONFIG = None
+CATEGORIES = list(dict.fromkeys([i._category for i in fast.conf.PARAMS]).keys())
+ELEMENTS = {}
+
+
+def load_config(e: events.UploadEventArguments):
+    content = e.content.read().decode()
+    exec(content, locals())
+    CONFIG = fast.conf.ConfigParser(locals()['p']).config
+    print(CONFIG)
+
+
+def add_number_box_bounds(param):
+    v = None
+    step = None
+    if isinstance(param.value, (float, int)):
+        v = param.value
+        step = param.value/10
+    return ui.number(param.name, value=v, suffix=param.unit, min=param._bounds[0], max=param._bounds[1], step=step)
+
+
+def add_array_box(param):
+    boxes = []
+    for i, v in enumerate(param.value):
+        boxes.append(ui.number(f"{param.name}{i}", value=v, suffix=param.unit, min=param._bounds[0], max=param._bounds[1], step=max(param.value)/10))
+    return boxes
+
+
+def add_param(param):
+    elements = []
+
+    with ui.row():
+
+        # some params need special treatment, do that here 
+        if param.name in ["NPXLS", "DX"]:
+            box = add_number_box_bounds(param)
+            checkbox = ui.checkbox("auto", value=(param.value=="auto"))
+            return [box, checkbox]
+        
+        if param.name in ["W0"]:
+            box = add_number_box_bounds(param)
+            checkbox = ui.checkbox("opt", value=(param.value=="opt"))
+            return [box, checkbox]
+        
+        if param.name in ["LOGFILE"]:
+            box = ui.input(param.name)
+            checkbox = ui.checkbox("None", value=(param.value==None))
+            return [box, checkbox]
+        
+        if param.name in ["SEED"]:
+            box = add_number_box_bounds(param)
+            checkbox = ui.checkbox("None", value=(param.value==None))
+            return [box, checkbox]
+    
+
+        # check through different types of params
+        if isinstance(param.value, bool):
+            sw = ui.switch(param.name, value=param.value)
+            elements.append(sw)
+        
+        elif isinstance(param.value, str):
+            ui.label(param.name)
+            rad = ui.radio(param._allowed_values, value=param.value).props("inline")
+            elements.append(rad)
+
+        elif isinstance(param.value, (float,int)):
+            if param.value < numpy.inf:
+                box = add_number_box_bounds(param)
+                elements.append(box)
+            else:
+                box = ui.number(param.name, suffix=param.unit, min=param._bounds[0], max=param._bounds[1])
+                cbox = ui.checkbox("inf", value=True)
+                elements.append(box)
+                elements.append(cbox)
+                
+            if len(param._allowed_values) > 0:
+                for allowed in param._allowed_values:
+                    box1 = ui.checkbox(str(allowed), value=False)
+                    elements.append(box1)
+
+        elif isinstance(param.value, (numpy.ndarray, list)):
+            boxes = add_array_box(param)
+            elements = boxes
+
+        elif param.value is None:
+            if param._bounds != [-numpy.inf, numpy.inf]:
+                # probably a number 
+                box = ui.number(param.name, min=param._bounds[0], max=param._bounds[1])
+                cbox = ui.checkbox("None", value=True)
+                elements.append(box)
+                elements.append(cbox)
+            else:
+                if len(param._allowed_values) > 0:
+                    ui.label(param.name)
+                    rad = ui.radio(param._allowed_values, value=param.value).props("inline")
+                    elements.append(rad)
+
+
+        else:
+            # anything left over? 
+            raise Exception(f"Param {param.name} not included in gui init")
+            
+        return elements
+    
+
+def handle_change(e, param):
+    print(e, param)
+
+
+with ui.tabs().classes("w-full") as tabs:
+    config_tab = ui.tab("Configuration")
+    sim_tab = ui.tab("Simulation")
+    results_tab = ui.tab("Results")
+
+with ui.tab_panels(tabs, value=config_tab).classes("w-full"):
+
+    with ui.tab_panel(config_tab):
+
+        with ui.column().classes("justify-center"):
+            ui.upload(label="Load Config File", on_upload=load_config).props('accept=.py')
+            
+            with ui.row():
+                
+                for c in CATEGORIES:
+
+                    with ui.card():
+
+                        ui.label(c)
+
+                        for param in fast.conf.PARAMS:
+
+                            if param._category == c:
+
+                                elems = add_param(param)
+                                ELEMENTS[param.name] = elems
+                                for elem in elems:
+                                    elem.on_value_change(lambda e, param=param: handle_change(e, param))
+
+
+    with ui.tab_panel(sim_tab):
+        ui.label("Simulation")
+
+    with ui.tab_panel(results_tab):
+        ui.label("Results")
+
+ui.run()
